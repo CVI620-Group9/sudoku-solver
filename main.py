8a7326d0fdd381675e03f494cea764843497d58a
hekmat-shrez <63696134+hekmat-shrez@users.noreply.github.com>
2021-11-17 20:21:32 -0500

    Text Detection 0-9

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import cv2 as cv
 import numpy as np
+from tensorflow.keras.models import load_model
+import pytesseract
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+model = load_model('./myModel.h5')
 
 #Process the Image to gray Color, and and return the Threshold Image
 def processImage(image):
@@ -16,7 +22,6 @@ def biggestContour(contour):
     for i in contour:
         count = count + 1
         area = cv.contourArea(i)
-        print("Area = ", area)
         if area > 60:
             peri = cv.arcLength(i, True)
             approx = cv.approxPolyDP(i, 0.02 * peri, True)
@@ -49,6 +54,27 @@ def splitBoxes(img):
             boxes.append(box)
     return boxes
 
+
+def getPredection(boxes,model):
+    output = []
+    for image in boxes:
+        ## PREPARE IMAGE
+        img = np.asarray(image)
+        img = img[4:img.shape[0] - 4, 4:img.shape[1] -4]
+        img = cv.resize(img, (28, 28))
+        img = img / 255
+        img = img.reshape(1, 28, 28, 1)
+        ## GET PREDICTION
+        predictions = model.predict(img)
+        classIndex = model.predict_classes(img)
+        predictedValue = np.amax(predictions)
+        ## SAVE TO RESULT
+        if predictedValue > 0.7:
+            output.append(classIndex[0])
+        else:
+            output.append(0)
+    return output
+
 img = cv.imread("sudoku.jpg")
 imgHeight = 450
 imgWidth = 450
@@ -75,7 +101,7 @@ if biggest.size != 0:
     cv.drawContours(imgBigContours, biggest, -1, (0, 0, 255), 3)
     pnts1 = np.float32(biggest)
     pnts2 = np.float32([[0,0], [imgWidth, 0], [0, imgHeight], [imgWidth, imgHeight]])
-    matrix = cv.getPerspectiveTransform(pnts1, pnts2)
+    matrix = cv.getPerspectiveTransform(pnts1 + 1, pnts2)
     imgWarpColored = cv.warpPerspective(img, matrix, (imgWidth, imgHeight))
     imgDetectedDigits = blankImg.copy()
     imgWarpColored = cv.cvtColor(imgWarpColored, cv.COLOR_BGR2GRAY)
@@ -84,10 +110,18 @@ if biggest.size != 0:
 boxes = splitBoxes(imgWarpColored)
 print("Boxes length",len(boxes))
 
+numbers = getPredection(boxes, model)
+print(numbers)
+
+
 cv.imshow("Sudoko Image", img)
 cv.imshow("Threshold Image", imgThres)
 cv.imshow("All Image Contours", imgContours)
 cv.imshow("biggest Countour Points", imgBigContours)
 cv.imshow("Image warp", imgWarpColored)
-cv.imshow("boxes at 2", boxes[11])
+cv.imshow("boxes", boxes[0])
 cv.waitKey(0)
+
+
+
+
