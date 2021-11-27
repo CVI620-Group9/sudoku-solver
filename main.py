cc4c4e4774562ff540c2caf71f8514b7e1129a90
hekmat-shrez <63696134+hekmat-shrez@users.noreply.github.com>
2021-11-26 21:46:13 -0500

    modularize the main.py

--- main.py
+++ main.py
@@ -1,79 +1,10 @@
 import cv2 as cv
 import numpy as np
-from tensorflow.keras.models import load_model
-import pytesseract
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-
-model = load_model('./myModel.h5')
-
-#Process the Image to gray Color, and and return the Threshold Image
-def processImage(image):
-    grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    blurImage = cv.GaussianBlur(grayImage, (5,5), cv.BORDER_DEFAULT)
-    imgThreshold = cv.adaptiveThreshold(blurImage, 255, 1, 1, 11, 2)
-    return imgThreshold
-
-def biggestContour(contour):
-    biggest = np.array([])
-    max_area = 0
-    count = 1
-
-    for i in contour:
-        count = count + 1
-        area = cv.contourArea(i)
-        if area > 60:
-            peri = cv.arcLength(i, True)
-            approx = cv.approxPolyDP(i, 0.02 * peri, True)
-            if area > max_area and len(approx) == 4:
-                biggest = approx
-                max_area = area
-    print(count)
-    return biggest, max_area
+import modules.utils as utils
 
 
-# Reorder the The biggest Contour cordinate points
-def reorder(points):
-    points = points.reshape((4, 2))
-    newPoints = np.zeros((4, 1, 2), dtype=np.int32)
-    add = points.sum(1)
-    newPoints[0] = points[np.argmin(add)]
-    newPoints[3] = points[np.argmax(add)]
-    diff = np.diff(points, axis=1)
-    newPoints[1] = points[np.argmin(diff)]
-    newPoints[2] = points[np.argmax(diff)]
-    return newPoints
-
-# split the warp image for each digit box
-def splitBoxes(img):
-    rows = np.vsplit(img, 9)
-    boxes = []
-    for r in rows:
-        cols = np.hsplit(r , 9)
-        for box in cols:
-            boxes.append(box)
-    return boxes
-
-
-def getPredection(boxes,model):
-    output = []
-    for image in boxes:
-        ## PREPARE IMAGE
-        img = np.asarray(image)
-        img = img[4:img.shape[0] - 4, 4:img.shape[1] -4]
-        img = cv.resize(img, (28, 28))
-        img = img / 255
-        img = img.reshape(1, 28, 28, 1)
-        ## GET PREDICTION
-        predictions = model.predict(img)
-        classIndex = model.predict_classes(img)
-        predictedValue = np.amax(predictions)
-        ## SAVE TO RESULT
-        if predictedValue > 0.7:
-            output.append(classIndex[0])
-        else:
-            output.append(0)
-    return output
 
 img = cv.imread("sudoku.jpg")
 imgHeight = 450
@@ -81,7 +12,7 @@ imgWidth = 450
 
 img = cv.resize(img, (imgWidth, imgHeight))
 
-imgThres = processImage(img)
+imgThres = utils.processImage(img)
 
 # Find all the Contours
 imgContours = img.copy()
@@ -92,12 +23,11 @@ cv.drawContours(imgContours, contours, -1, (0, 255, 0), 3)
 blankImg = np.zeros((imgHeight, imgWidth, 3), np.uint8 )
 
 # Find the bigest contours, and warp it
-biggest, maxArea = biggestContour(contours)
+biggest, maxArea = utils.biggestContour(contours)
 cv.drawContours(imgBigContours, biggest, -1, (0, 0, 255), 10)
-print(biggest, "Max Area = ", maxArea)
+
 if biggest.size != 0:
-    biggest = reorder(biggest)
-    print(biggest)
+    biggest = utils.reorder(biggest)
     cv.drawContours(imgBigContours, biggest, -1, (0, 0, 255), 3)
     pnts1 = np.float32(biggest)
     pnts2 = np.float32([[0,0], [imgWidth, 0], [0, imgHeight], [imgWidth, imgHeight]])
@@ -107,10 +37,9 @@ if biggest.size != 0:
     imgWarpColored = cv.cvtColor(imgWarpColored, cv.COLOR_BGR2GRAY)
 
 # Split The Digit
-boxes = splitBoxes(imgWarpColored)
-print("Boxes length",len(boxes))
+boxes = utils.splitBoxes(imgWarpColored)
 
-numbers = getPredection(boxes, model)
+numbers = utils.getPredection(boxes, utils.model)
 print(numbers)
 
 
--- modules/utils.py
+++ modules/utils.py
@@ -1,6 +1,8 @@
 import cv2 as cv
+import numpy as np
 
 from modules.DrawInfo import DrawInfo
+from tensorflow.keras.models import load_model
 
 def showAndSave(image, imageName, isSaving):
     '''
@@ -28,9 +30,80 @@ def showAndSave(image, imageName, isSaving):
     # Specifically destroy the window of the new image
     cv.destroyWindow(f"{imageName}")
 
+model = load_model('../myModel.h5')
+
+#Process the Image to gray Color, and and return the Threshold Image
+def processImage(image):
+    grayImage = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+    blurImage = cv.GaussianBlur(grayImage, (5,5), cv.BORDER_DEFAULT)
+    imgThreshold = cv.adaptiveThreshold(blurImage, 255, 1, 1, 11, 2)
+    return imgThreshold
+
+def biggestContour(contour):
+    biggest = np.array([])
+    max_area = 0
+    count = 1
+
+    for i in contour:
+        count = count + 1
+        area = cv.contourArea(i)
+        if area > 60:
+            peri = cv.arcLength(i, True)
+            approx = cv.approxPolyDP(i, 0.02 * peri, True)
+            if area > max_area and len(approx) == 4:
+                biggest = approx
+                max_area = area
+    print(count)
+    return biggest, max_area
+
+
+# Reorder the The biggest Contour cordinate points
+def reorder(points):
+    points = points.reshape((4, 2))
+    newPoints = np.zeros((4, 1, 2), dtype=np.int32)
+    add = points.sum(1)
+    newPoints[0] = points[np.argmin(add)]
+    newPoints[3] = points[np.argmax(add)]
+    diff = np.diff(points, axis=1)
+    newPoints[1] = points[np.argmin(diff)]
+    newPoints[2] = points[np.argmax(diff)]
+    return newPoints
+
+# split the warp image for each digit box
+def splitBoxes(img):
+    rows = np.vsplit(img, 9)
+    boxes = []
+    for r in rows:
+        cols = np.hsplit(r , 9)
+        for box in cols:
+            boxes.append(box)
+    return boxes
+
+
+def getPredection(boxes,model):
+    output = []
+    for image in boxes:
+        ## PREPARE IMAGE
+        img = np.asarray(image)
+        img = img[4:img.shape[0] - 4, 4:img.shape[1] -4]
+        img = cv.resize(img, (28, 28))
+        img = img / 255
+        img = img.reshape(1, 28, 28, 1)
+        ## GET PREDICTION
+        predictions = model.predict(img)
+        classIndex = model.predict_classes(img)
+        predictedValue = np.amax(predictions)
+        ## SAVE TO RESULT
+        if predictedValue > 0.7:
+            output.append(classIndex[0])
+        else:
+            output.append(0)
+    return output
+
 def drawRectangle(drawInfoObject, image, colour):
         if isinstance(drawInfoObject, DrawInfo):
             cv.rectangle(image, (drawInfoObject.x1, drawInfoObject.y1), (drawInfoObject.x2, drawInfoObject.y2), colour, 2)
+
 
 from enum import Enum
 # Colour Enum
